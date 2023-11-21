e5b1269182ff0782c0bbfb33f57cc43ab266e391
Arantza Castro <ary.castro.dessavre@gmail.com>
2023-11-21 16:20:31 +0100

    Idk

---
+++ NC1_SwiftData/RecipeCardView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeCardView.swift
+//  NC1_SwiftData
+//
+//  Created by Arantza Castro Dessavre on 21/11/23.
+//
+
+import SwiftUI
+
+struct RecipeCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeCardView()
+}
